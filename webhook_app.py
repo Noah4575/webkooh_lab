7ed0d6b4ec886b1b630e0fb55b5863ab5e6abe6d
Noah4575 <73112285+Noah4575@users.noreply.github.com>
2024-01-17 09:28:35 +0100

    Update webhook_app.py

--- webhook_app.py
+++ webhook_app.py
@@ -1,1 +1,36 @@
-print('h')
+from flask import Flask, request
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/staging', methods=['POST'])
+def staging():
+    payload = request.json
+    ref = payload.get('ref', '')
+
+    # Check if the push is to the desired branch, e.g., 'refs/heads/testing'
+    if ref == 'refs/heads/staging':
+        # this hook is coming from a push done to the "testing" branch
+        # Add your code logic here
+        subprocess.run(['./staging_script.sh'])
+        return 'OK', 200
+    else:
+        return 'Skip',200
+    
+@app.route('/deploy', methods=['POST'])
+
+def deploy():
+    payload = request.json
+    ref = payload.get('ref', '')
+
+    # Check if the push is to the desired branch, e.g., 'refs/heads/testing'
+    if ref == 'refs/heads/deploy':
+        # this hook is coming from a push done to the "testing" branch
+        # Add your code logic here
+        subprocess.run(['./deploy_script.sh'])
+        return 'OK', 200
+    else:
+        return 'Skip',200
+    
+if __name__ == '__main__':
+    app.run(debug=True,port=5000)
